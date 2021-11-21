ea8949cf60b335cc3f08fafc6b5a0999f793ccff
waqxxmalik <malikwaqxx@gmail.com>
2021-11-21 14:58:59 -0800

    ds

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+	
+
+app.use(express.static('public'))
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "public"));
+
+app.get("/", function(req, res){
+	res.render("index")
+})
+app.listen(process.env.PORT || 4050, () => {
+  console.log(`getting started`);
+});
